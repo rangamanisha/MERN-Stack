152ede687a8c5698b86c3694eeb5cfed72bbb22d
rangamanisha <manisharanga11@gmail.com>
2024-12-17 15:04:45 -0500

    POST request to post comments on articles

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,20 +1,35 @@
 import express from 'express';
 
 //im memory data base to store or keep tracks of upvotes/comments of each artciles
-const artcileInfo = [
-    {name: 'learn-node', upvotes:0},
-    {name: 'learn-react', upvotes:0},
-    {name: 'mongodb', upvote:0}
+const articleInfo = [
+    {name: 'learn-node', upvotes:0, comments: []},
+    {name: 'learn-react', upvotes:0, comments: []},
+    {name: 'mongodb', upvote:0, comments:[]}
 ]
 const app = express();
 app.use(express.json());
 
 app.post('/api/articles/:name/upvote', (req, res) => {
-    const article = artcileInfo.find(a => a.name === req.params.name);
+    const article = articleInfo.find(a => a.name === req.params.name);
     article.upvotes +=1;
 
-    res.send('Success! The article' + req.params.name + 'now has ' + article.upvotes + ' upvotes!' )
+    res.json(article)
+
 })
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const {name} = req.params;
+    const {postedBy, text} = req.body;
+
+    const article = articleInfo.find(a => a.name === name);
+
+    article.comments.push({
+        postedBy,
+        text,
+    })
+
+    res.json(article);
+} )
 
 // app.get('/hello', function(req, res) {
 // });
